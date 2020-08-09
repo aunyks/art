a7431b933c2be3d1d233cb6712d151587c888959
Gerald Nash <me@aunyks.com>
2020-08-09 14:58:05 -0400

    New art site yessir

---
+++ pages/2020/index.js
@@ -0,0 +1,10 @@
+import LinksPage from 'components/LinksPage'
+import Link from 'components/Link'
+
+export default () => (
+  <LinksPage
+    pageTitle="2020 Art Pieces"
+    title="2020 art pieces">
+    <Link href="#">coming soon</Link>
+  </LinksPage>
+)
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import LinksPage from 'components/LinksPage'
+import Link from 'components/Link'
+
+export default () => (
+  <LinksPage
+    pageTitle="Art"
+    title="art">
+    <Link href="https://aunyks.com/about">about me</Link>
+    <Link href="/2020">2020</Link>
+    <Link href="https://aunyks.com/art">old projects</Link>
+  </LinksPage>
+)
