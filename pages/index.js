549c32da8805c0aeb6c1c69809b3185e924dc1f6
Gerald Nash <me@aunyks.com>
2020-08-19 17:26:00 -0400

    ,

--- pages/index.js
+++ pages/index.js
@@ -8,5 +8,6 @@ export default () => (
     <Link href="https://aunyks.com/about">about me</Link>
     <Link href="/2020">2020</Link>
     <Link href="https://aunyks.com/art">old projects</Link>
+    <Link href="https://instagram.com/art.aunyks">instagram</Link>
   </LinksPage>
 )
