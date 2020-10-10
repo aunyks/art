0dfe3665bf8e0ceebe1ca31695175cafdab41102
Gerald Nash <me@aunyks.com>
2020-10-09 23:53:52 -0400

    Add Bridge To

---
+++ pages/2020/bridge-to.js
@@ -0,0 +1,137 @@
+import Head from 'next/head'
+import DefaultHead from 'components/DefaultHead'
+
+export default function BridgeTo() {
+  return (
+    <>
+      <DefaultHead
+        title="Bridge To"
+        description="Step onto The Bridge To."
+      />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <title>Bridge To</title>
+        <script src="/js/three/three.mod.js"></script>
+        <script src="/js/three/three.doc.js"></script>
+        <script src="/js/three/three.plc.js"></script>
+        <script src="/js/three/webgl.js"></script>
+        <script src="/js/bridge-to.js"></script>
+      </Head>
+      <style jsx global>{`
+        :root {
+          --light-bg-color: white;
+          --light-txt-color: black;
+          --dark-bg-color: rgb(40, 40, 40);
+          --dark-txt-color: white;
+        }
+        
+        html, body {
+          font-family: sans-serif;
+          background: var(--light-bg-color);
+          color: var(--light-txt-color);
+          width: 100vw;
+          height: 100vh;
+          margin: 0;
+        }
+        
+        @media (prefers-color-scheme: dark) {
+          html, body {
+            background: var(--dark-bg-color);
+            color: var(--dark-txt-color);
+          }
+        }
+
+        canvas {
+          display: block;
+          width: 100vw;
+          height: 100vh;
+        }
+        
+        #container {
+          padding: 0;
+          margin: 0;
+        }
+        
+        a {
+          color: inherit;
+        }
+        
+        #preface-text {
+          margin-top: 20%;
+          display: flex;
+          flex-direction: column;
+          padding: 10vh 10vw;
+          height: 100vh;
+        }
+        
+        #preface-text>h1 {
+          font-size: 3em;
+        }
+        
+        #startbtn {
+          width: 100%;
+          background: inherit;
+          color: inherit;
+          padding: 1em 2em;
+          font-size: 2em;
+          border-color: var(--light-txt-color);
+          border-width: 7px;
+          border-radius: 10px;
+          font-weight: bold;
+        }
+        
+        #author {
+          display: none;
+          text-decoration: none;
+          text-align: right;
+          color: inherit;
+        }
+        
+        #author:visited {
+          color: inherit;
+        }
+        
+        @media (prefers-color-scheme: dark) {
+        
+          html,
+          body {
+            background: var(--dark-bg-color);
+            color: var(--dark-txt-color);
+          }
+        
+          #startbtn {
+            border-color: var(--dark-txt-color);
+          }
+        }
+        
+        @media only screen and (max-width: 375px) {
+          #preface-text>h1 {
+            font-size: 2em;
+          }
+        
+          #startbtn {
+            font-size: 1em;
+          }
+        
+          #author {
+            font-size: 1em;
+          }
+        }
+        
+        @media only screen and (max-width: 320px) {
+          #preface-text>h1 {
+            font-size: 1.5em;
+          }
+        }
+      `}</style>
+      <div id="preface-text">
+        <h1>
+          Step onto The Bridge To.
+      </h1>
+        <button id="startbtn">Embark</button>
+        <a id="author" href="https://twitter.com/aunyks">@aunyks</a>
+      </div>
+      <div id="container"></div>
+    </>
+  )
+}
---
+++ public/js/bridge-to.js
@@ -0,0 +1,80 @@
+window.addEventListener('load', () => {
+  const isMobile = window.matchMedia('(max-width: 375px)').matches
+  let controls
+  let renderer
+  let scene
+  let camera
+  let pivot
+  let immersiveMode = false
+  const startBtn = document.getElementById('startbtn')
+  const init = () => {
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1100)
+    scene = new THREE.Scene()
+    if (isMobile) {
+      controls = new THREE.DeviceOrientationControls(camera)
+    } else {
+      controls = new THREE.PointerLockControls(camera, document.body)
+      controls.addEventListener('unlock', () => {
+        document.body.addEventListener('click', () => {
+          controls.lock()
+        })
+      })
+      scene.add(controls.getObject())
+    }
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    const loader = new THREE.TextureLoader()
+    const texture = loader.load('/assets/img/bridge-to/bridge-equirect.png', () => {
+      const rt = new THREE.WebGLCubeRenderTarget(texture.image.height)
+      rt.fromEquirectangularTexture(renderer, texture)
+      scene.background = rt
+    })
+    document.getElementById('container').appendChild(renderer.domElement)
+    window.addEventListener('resize', onWindowResize, false)
+  }
+
+  const animate = () => {
+    window.requestAnimationFrame(animate)
+    if (immersiveMode) {
+      controls.update()
+    }
+    renderer.render(scene, camera)
+  }
+
+  const onWindowResize = () => {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+  }
+
+  startBtn
+    .addEventListener('click', async () => {
+      // In immersive mode we read the (mobile) device orientation
+      // to move the camera, like a VR device!
+      // When not in it, we just use PointerLockControls to move the 
+      // camera like a PC first-person shooter
+      if (typeof DeviceOrientationEvent !== 'undefined' && typeof DeviceOrientationEvent.requestPermission === 'function') {
+        alert('To use immersive mode we\'re about to ask for permission to read your device\'s orientation!')
+        const permission = await DeviceOrientationEvent.requestPermission()
+        if (permission === 'granted') {
+          console.log('Orientation mode permission granted')
+          immersiveMode = true
+        } else {
+          console.log('Orientation mode permission not granted.')
+        }
+      } else {
+        console.log('Device doesn\'t allow orientation access.')
+      }
+      if (isMobile && !immersiveMode) {
+        alert('Aw..you didn\'t give us access. Okay, try again later!')
+        return
+      }
+      document.getElementById('preface-text').remove()
+      init()
+      if (!immersiveMode) {
+        controls.lock()
+      }
+      animate()
+    })
+})
